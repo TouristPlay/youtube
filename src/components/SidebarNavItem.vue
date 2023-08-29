0cb2a980046107a6905628acfaa901c7f069b90f
TouristPlay <touristtechology@gmail.com>
2023-08-29 22:52:15 +0300

    create component in sidebar

---
+++ src/components/SidebarNavItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import BaseIcon from "./BaseIcon.vue";
+import {computed} from "vue";
+
+const props = defineProps({
+    icon: String,
+    label: String,
+    isActive: Boolean
+})
+
+const classes = computed(() => {
+    const classes = ["flex", 'items-center', 'px-6', 'py-2', 'text-sm'];
+
+    return props.isActive
+        ? [...classes, 'font-medium', 'text-gray-800', "bg-gray-200", "hover:bg-gray-300"]
+        : [...classes, 'hover:bg-gray-100']
+});
+
+const iconClasses = computed(() => {
+    return props.isActive ? 'w-6 h-6 mr-6 text-red-500' : 'w-6 h-6 mr-6';
+});
+
+
+</script>
+
+<template>
+    <li>
+        <a href="#"
+           :class="classes">
+            <BaseIcon
+                :class="iconClasses"
+                :name="icon"
+            />
+            <span>{{ label }}</span>
+        </a>
+    </li>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/VideoItemInfo.vue
+++ src/components/VideoItemInfo.vue
@@ -1,11 +1,18 @@
 <script setup>
 
 import BaseIcon from "./BaseIcon.vue";
+import {computed} from "vue";
 
 const props = defineProps({
     index: Number
 })
 
+const summary = computed(() => {
+
+    const days = props.index === 1 ? 'day' : 'days';
+
+    return `${props.index}K views &middot; ${props.index} ${days} ago`;
+})
 
 </script>
 
@@ -13,7 +20,7 @@ const props = defineProps({
     <div class="flex items-start mt-3">
         <img src="../assets/images/channel/channel.jpg" class="mr-3 rounded-full w-9 h-9" alt="">
         <div class="text-sm">
-            <span class="font-semibold text-gray-800">Video title {{index}}</span>
+            <span class="font-semibold text-gray-800">Video title {{ index }}</span>
             <div class="mt-1 flex items-center">
                 <span>Channel name</span>
                 <BaseIcon
@@ -21,9 +28,7 @@ const props = defineProps({
                     class="w-3.5 h-3.5 ml-1"
                 />
             </div>
-            <div>
-                {{index}}K views &middot; {{index}} day ago
-            </div>
+            <div v-html="summary"></div>
         </div>
         <button
             class="-mt-1 ml-auto p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none">
