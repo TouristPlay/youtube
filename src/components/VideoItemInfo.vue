4b45eb2eca2e915d848fa04097e80c3965d6f1e1
TouristPlay <touristtechology@gmail.com>
2023-08-29 22:15:43 +0300

    create component in videoItem

---
+++ src/components/VideoItemInfo.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import BaseIcon from "./BaseIcon.vue";
+
+const props = defineProps({
+    index: Number
+})
+
+
+</script>
+
+<template>
+    <div class="flex items-start mt-3">
+        <img src="../assets/images/channel/channel.jpg" class="mr-3 rounded-full w-9 h-9" alt="">
+        <div class="text-sm">
+            <span class="font-semibold text-gray-800">Video title {{index}}</span>
+            <div class="mt-1 flex items-center">
+                <span>Channel name</span>
+                <BaseIcon
+                    name="checkCircle"
+                    class="w-3.5 h-3.5 ml-1"
+                />
+            </div>
+            <div>
+                {{index}}K views &middot; {{index}} day ago
+            </div>
+        </div>
+        <button
+            class="-mt-1 ml-auto p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none">
+            <BaseIcon
+                name="dostVertical"
+            />
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/VideoItemThumbnail.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+    import BaseIcon from "./BaseIcon.vue";
+
+    const props = defineProps({
+        index: Number
+    })
+
+</script>
+
+<template>
+    <div class="relative">
+        <img src="../assets/images/video/video.jpg">
+        <span
+            class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-0 right-0 bg-black text-white rounded-sm m-1 p-1">
+            <BaseIcon
+                name="clock"
+                class="w-5 h-5"
+            />
+          </span>
+        <span
+            class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-8 right-0 bg-black text-white rounded-sm m-1 p-1">
+            <BaseIcon
+                name="menuAlt3"
+                class="w-5 h-5"
+            />
+          </span>
+        <span
+            class="opacity-100 group-hover:opacity-0 duration-500 absolute bottom-0 right-0 bg-black text-white rounded-sm m-1 p-1 text-xs font-semibold">6:35</span>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
